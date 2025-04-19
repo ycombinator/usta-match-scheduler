f29ad61d8e6e9c054e3a158f4ec9a11c8130abb7
Shaunak Kashyap <ycombinator@gmail.com>
2025-04-19 08:04:17 -0700

    Adding web server

---
+++ web/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const proxy = require('express-http-proxy');
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+const app = express();
+
+
+app.use('/api', proxy("localhost:8000", {
+    proxyReqPathResolver: (req) => {
+        return req.url;
+    },
+}));
+
+app.use(express.static("dist"))
+// app.use(webpackDevMiddleware(compiler, {
+//   publicPath: config.devServer.static.directory
+// }));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
