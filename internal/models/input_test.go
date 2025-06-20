da7c579d8ee24ac0ea56382c903277c81f80f86c
Shaunak Kashyap <ycombinator@gmail.com>
2025-06-20 09:13:22 -0700

    Fix unit tests

--- internal/models/input_test.go
+++ internal/models/input_test.go
@@ -1,45 +1,40 @@
 package models
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFirstDayOfMatches(t *testing.T) {
 	cases := map[string]struct {
-		teams       map[string]Team
-		expected    func() *time.Time
+		teams       map[string]SchedulingTeam
+		expected    *time.Time
 		expectedErr error
 	}{
 		"empty": {
 			teams:       nil,
-			expected:    func() *time.Time { return nil },
+			expected:    nil,
 			expectedErr: nil,
 		},
 		"one_date_per_team": {
-			teams: map[string]Team{
+			teams: map[string]SchedulingTeam{
 				"w3.5": {Weeks: []string{"20241028"}},
 				"m3.5": {Weeks: []string{"20241021"}},
 				"m4.5": {Weeks: []string{"20241021"}},
 				"w2.5": {Weeks: []string{"20241104"}},
 			},
-			expected: func() *time.Time {
-				t := time.Date(2024, 10, 21, 0, 0, 0, 0, time.UTC)
-				return &t
-			},
+			expected: ptrTo[time.Time](time.Date(2024, 10, 21, 0, 0, 0, 0, time.UTC)),
 		},
 		"multiple_date_per_team": {
-			teams: map[string]Team{
+			teams: map[string]SchedulingTeam{
 				"w3.5": {Weeks: []string{"20241021", "20241028"}},
 				"m3.5": {Weeks: []string{"20241021", "20241014"}},
 				"m4.5": {Weeks: []string{"20241021", "20241104"}},
 				"w2.5": {Weeks: []string{"20241104"}},
 			},
-			expected: func() *time.Time {
-				t := time.Date(2024, 10, 14, 0, 0, 0, 0, time.UTC)
-				return &t
-			},
+			expected: ptrTo[time.Time](time.Date(2024, 10, 14, 0, 0, 0, 0, time.UTC)),
 		},
 	}
 
@@ -48,7 +43,7 @@ func TestFirstDayOfMatches(t *testing.T) {
 			input := Input{Teams: test.teams}
 			firstDayOfMatches, err := input.FirstDayOfMatches()
 
-			require.Equal(t, test.expected(), firstDayOfMatches)
+			require.Equal(t, test.expected, firstDayOfMatches)
 			require.Equal(t, test.expectedErr, err)
 		})
 	}
@@ -56,38 +51,32 @@ func TestFirstDayOfMatches(t *testing.T) {
 
 func TestLastDayOfMatches(t *testing.T) {
 	cases := map[string]struct {
-		teams       map[string]Team
-		expected    func() *time.Time
+		teams       map[string]SchedulingTeam
+		expected    *time.Time
 		expectedErr error
 	}{
 		"empty": {
 			teams:       nil,
-			expected:    func() *time.Time { return nil },
+			expected:    nil,
 			expectedErr: nil,
 		},
 		"one_date_per_team": {
-			teams: map[string]Team{
+			teams: map[string]SchedulingTeam{
 				"w3.5": {Weeks: []string{"20241028"}},
 				"m3.5": {Weeks: []string{"20241104"}},
 				"m4.5": {Weeks: []string{"20241021"}},
 				"w2.5": {Weeks: []string{"20241028"}},
 			},
-			expected: func() *time.Time {
-				t := time.Date(2024, 11, 10, 0, 0, 0, 0, time.UTC)
-				return &t
-			},
+			expected: ptrTo[time.Time](time.Date(2024, 11, 10, 0, 0, 0, 0, time.UTC)),
 		},
 		"multiple_date_per_team": {
-			teams: map[string]Team{
+			teams: map[string]SchedulingTeam{
 				"w3.5": {Weeks: []string{"20241021", "20241028"}},
 				"m3.5": {Weeks: []string{"20241021", "20241111"}},
 				"m4.5": {Weeks: []string{"20241021", "20241104"}},
 				"w2.5": {Weeks: []string{"20241104"}},
 			},
-			expected: func() *time.Time {
-				t := time.Date(2024, 11, 17, 0, 0, 0, 0, time.UTC)
-				return &t
-			},
+			expected: ptrTo[time.Time](time.Date(2024, 11, 17, 0, 0, 0, 0, time.UTC)),
 		},
 	}
 
@@ -96,8 +85,12 @@ func TestLastDayOfMatches(t *testing.T) {
 			input := Input{Teams: test.teams}
 			lastDayOfMatches, err := input.LastDayOfMatches()
 
-			require.Equal(t, test.expected(), lastDayOfMatches)
+			require.Equal(t, test.expected, lastDayOfMatches)
 			require.Equal(t, test.expectedErr, err)
 		})
 	}
+}
+
+func ptrTo[T any](v T) *T {
+	return &v
 }
