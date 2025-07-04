79cf5ad8e163b7c0ecc23ce2f5fb040d0a498367
Shaunak Kashyap <ycombinator@gmail.com>
2025-07-04 13:32:44 -0700

    Making development easier

---
+++ README.md
@@ -0,0 +1,12 @@
+## Development
+
+To run the application in development mode, you can use the following command:
+
+```bash
+docker compose up --watch
+```
+
+This command will start the application and watch for changes in the source code, automatically 
+rebuilding and restarting the application as needed.
+
+The application will be accessible at `http://localhost:3000`.
--- web/server.js
+++ web/server.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
 
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-
 const app = express();
 
-app.use('/api', proxy("localhost:8000", {
+const apiHostPort = process.env.API_HOST_PORT || "localhost:8000"
+app.use('/api', proxy(apiHostPort, {
     proxyReqPathResolver: (req) => {
         return req.url;
     },
 }));
 
 app.use(express.static("dist"))
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.devServer.static.directory
-}));
 
 app.listen(3000, function () {
   console.log('Application listening on port 3000!');
